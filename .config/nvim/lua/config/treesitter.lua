4be355eeb109b196bde48cf02e10354237ecfe00
joey364 <71462893+joey364@users.noreply.github.com>
2022-06-14 10:29:16 +0000

    nvim: add ensure_installed table for nvim-treesitter

--- .config/nvim/lua/config/treesitter.lua
+++ .config/nvim/lua/config/treesitter.lua
@@ -4,7 +4,25 @@ if not status_ok then
 end
 
 configs.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = {
+    "bash",
+    "c",
+    "cpp",
+    "css",
+    "go",
+    "html",
+    "java",
+    "javascript",
+    "json",
+    "lua",
+    "python",
+    "regex",
+    "rust",
+    "tsx",
+    "typescript",
+    "yaml",
+  },
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "" }, -- List of parsers to ignore installing
   highlight = {
